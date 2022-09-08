2d7a2fc82afb910899ddfab5fa641a86c6a4be2d
clerostyle <glazacevnikolaj7@gmail.com>
2022-09-08 22:12:08 +0300

    Fix lesson3

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -83,10 +83,10 @@ fun digitNumber(n: Int): Int = TODO()
  * Ряд Фибоначчи определён следующим образом: fib(1) = 1, fib(2) = 1, fib(n+2) = fib(n) + fib(n+1)
  */
 fun fib(n: Int): Int {
-    var result = 0
-    var nextResult = 1
+    var result = 1
+    var nextResult = 0
     var postNextResult = 1
-    for (i in 1..n) {
+    for (i in 0..n) {
         val tempNextResult = nextResult
         val tempPostNextResult = postNextResult
         postNextResult += nextResult
@@ -207,24 +207,23 @@ fun cos(x: Double, eps: Double): Double = TODO()
  * Использовать операции со строками в этой задаче запрещается.
  */
 fun squareSequenceDigit(n: Int): Int {
-    var counter = 0
-    for (i in 1..n * 10000000) {
-        var number = sqr(i)
-        var squareNumber = revertNumber(number)
-
-        while (squareNumber != 0) {
+    var counter = 1
+    for (i in 1..Int.MAX_VALUE) {
+        var number = i * i
+        var revertedNumber = revertNumber(number)
+        while (revertedNumber != 0) {
+            if (counter == n) return revertedNumber % 10
+            revertedNumber /= 10
             counter++
-            if (n == counter) return squareNumber % 10
-            squareNumber /= 10
         }
 
         while (number % 10 == 0) {
-            counter++
-            if (n == counter) return 0
+            if (counter == n) return number % 10
             number /= 10
+            counter++
         }
     }
-    return 1
+    return 0
 }
 
 /**
@@ -238,6 +237,7 @@ fun squareSequenceDigit(n: Int): Int {
  */
 fun fibSequenceDigit(n: Int): Int {
     var counter = 0
+    println("${fib(0)}")
     for (i in 1..n) {
         var fibNumber = revertNumber(fib(i))
 
